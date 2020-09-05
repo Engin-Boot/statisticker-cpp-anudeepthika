1f6d6e73b9e2b295b6240ecb38e8ae9d943bc09d
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-05 16:34:18 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,9 +1,78 @@
+#include <bits/stdc++.h>
 #include "stats.h"
-namespace Statistics{
-    
+double calculateAverage(const std::vector<double>& data)
+{
+    double sum = 0;
+    int noofdata = data.size();
+    for(double itr : data)
+        sum = sum + itr;
+    return sum/noofdata;
+}
 
+double findMax(const std::vector<double>& data)
+{
+    double max = INT_MIN;
+    for(double itr : data)
+    {
+        if(itr>max)
+            max = itr;
+    }
+    return max;
+}
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+double findMin(const std::vector<double>& data)
+{
+    double min = INT_MAX;
+    for(double itr : data)
+    {
+        if(itr<min)
+            min = itr;
+    }
+    return min;
+}
+
+Stats Statistics:: ComputeStatistics(const std::vector<double>& numericaldata) 
+{
     //Implement statistics here
+    Stats stats;
+    int nancount=0;
+    int numbercount=0;
+    if(numericaldata.size()==0)
+    {
+        stats.average = std::nan("");
+        stats.max = std::nan("");
+        stats.min = std::nan("");
+        return stats;
+    }
+    else
+    {
+        for (auto& ptr : numericaldata) 
+        { 
+        if(isnan(ptr))
+            nancount++;
+        else
+            numbercount++;                
+        }
+        if(nancount>=numbercount)
+        {
+            stats.average = std::nan("");
+            stats.max = std::nan("");
+            stats.min = std::nan("");
+        }
+        else
+        {
+            std::vector<double> nanremoveddata;
+            for(double num : numericaldata)
+            {
+                if(!isnan(num))
+                    nanremoveddata.push_back(num);
+            }
+            stats.average = calculateAverage(nanremoveddata);
+            stats.max = findMax(nanremoveddata);
+            stats.max = findMin(nanremoveddata);
+        }
+        return stats;
+    }
+    
 }
-}
+
