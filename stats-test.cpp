1bba28058b5cf0dcac21db1490eb54dc45e2418f
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-05 18:03:48 +0530

    Update statstets

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,10 +2,14 @@
 
 #include "catch.hpp"
 #include "stats.h"
-
 #include <cmath>
-
-TEST_CASE("reports average, minimum and maximum") {
+//All fields of computedStats (average, max, min) must be
+    //NAN (not-a-number), as defined in math.h
+    
+    //Design the REQUIRE statement here.
+    //Use http://www.cplusplus.com/reference/cmath/isnan/
+TEST_CASE("reports average, minimum and maximum") 
+{
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     double epsilon = 0.001;
     REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
@@ -13,15 +17,27 @@ TEST_CASE("reports average, minimum and maximum") {
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
-TEST_CASE("average is NaN for empty array") {
+TEST_CASE("average is NaN for empty array") 
+{
     auto computedStats = Statistics::ComputeStatistics({});
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Design the REQUIRE statement here.
-    //Use http://www.cplusplus.com/reference/cmath/isnan/
     REQUIRE(std::isnan(computedStats.average));
     REQUIRE(std::isnan(computedStats.max));
-    REQUIRE(std::isnan(computedStats.min));
-    
+    REQUIRE(std::isnan(computedStats.min));   
+}
+
+TEST_CASE("reports average, minimum and maximum for array containing NaN input") 
+{
+    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9,nan(""), 3.2, 4.5,nan("")});
+    double epsilon = 0.001;
+    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
+    REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
+}
+
+TEST_CASE("average is NaN for an array containing NaN input greater than or equal to numbers") 
+{
+    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9,nan(""), nan(""), 4.5,nan(""),nan(""),nan("")});
+    REQUIRE(std::isnan(computedStats.average));
+    REQUIRE(std::isnan(computedStats.max));
+    REQUIRE(std::isnan(computedStats.min));   
 }
