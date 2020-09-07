498cffef52cb93e2f311cc22198fcfab8aa00719
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-07 21:24:03 +0530

    Update stats-test

--- stats-test.cpp
+++ stats-test.cpp
@@ -8,7 +8,7 @@
     
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
-TEST_CASE("reports average, minimum and maximum") 
+TEST_CASE("when inputs are not NaN then reports average, minimum and maximum of the data") 
 {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     double epsilon = 0.001;
@@ -17,15 +17,15 @@ TEST_CASE("reports average, minimum and maximum")
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
-TEST_CASE("average is NaN for empty array") 
+TEST_CASE("when the input is empty array then average, minimum and maximum are all NaN") 
 {
     auto computedStats = Statistics::ComputeStatistics({});
     REQUIRE(std::isnan(computedStats.average));
     REQUIRE(std::isnan(computedStats.max));
     REQUIRE(std::isnan(computedStats.min));   
 }
-
-TEST_CASE("reports average, minimum and maximum for array containing NaN input") 
+/*Having invalid data is acceptable to a certain limit*/
+TEST_CASE("when input contains NaN input and count of Nan is less than not NaN count then reports average, minimum and maximum for array removed NaN") 
 {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9,nan(""), 3.2, 4.5,nan("")});
     double epsilon = 0.001;
@@ -33,8 +33,8 @@ TEST_CASE("reports average, minimum and maximum for array containing NaN input")
     REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
-
-TEST_CASE("average is NaN for an array containing NaN input greater than or equal to numbers") 
+/*Too much of invalid data*/
+TEST_CASE("when an array contains NaN input greater than or equal to numbers, then average, minimum and maximum are all NaN") 
 {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9,nan(""), nan(""), 4.5,nan(""),nan(""),nan("")});
     REQUIRE(std::isnan(computedStats.average));
